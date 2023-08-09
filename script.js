9b56c2dd0ebc43f4f8a50c9577c1ee0cab10800e
Tarik Ziad <ziad2014.tt@gmail.com>
2023-08-09 04:16:43 +0100

    js done

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const pageOne = document.querySelector(".page1");
 const pageTwo = document.querySelector(".page2");
 const submitButton = document.getElementById("submit-rating");
-const ratings = document.querySelector(".btn");
+const ratings = document.querySelectorAll(".btn");
 const resultRating = document.getElementById("rating");
 
-    submitButton.addEventListener("click", () => {
-  pageOne.style.display = "none"
-  pageTwo.style.display = "block"
+  submitButton.addEventListener("click", () => {
+      pageOne.style.display = "none"
+      pageTwo.style.display = "block"
+  }) 
 
-  ratings.forEach((rating) => {
-    rating.addEventListener("click", () => {
-      resultRating.innerHTML = rating.innerHTML
+  ratings.forEach((rating) => {   
+      rating.addEventListener("click", () => {
+      resultRating.innerHTML = rating.innerHTML;
     })
   })
-})
